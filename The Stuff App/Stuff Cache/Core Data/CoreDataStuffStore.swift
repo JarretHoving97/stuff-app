84b2328811365da715f31c5aa6dac223301f7342
Jarret Hoving <jarret.hoving@wemamobile.nl>
2024-10-26 16:23:56 +0200

    added CoreData configuration

--- The Stuff App/Stuff Cache/Core Data/CoreDataStuffStore.swift
+++ The Stuff App/Stuff Cache/Core Data/CoreDataStuffStore.swift
@@ -9,4 +9,27 @@ import CoreData
 
 class CoreDataStuffStore {
     
+    private static let modelName = "Stuff"
+    private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataStuffStore.self))
+    
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentContainer(Error)
+    }
+    
+    public init(storeURL: URL) throws {
+        guard let model = CoreDataStuffStore.model else {
+            throw StoreError.modelNotFound
+        }
+        
+        do {
+            container = try NSPersistentContainer.load(name: CoreDataStuffStore.modelName, model: model, url: storeURL)
+            context = container.newBackgroundContext()
+        } catch {
+            throw StoreError.failedToLoadPersistentContainer(error)
+        }
+    }
 }
