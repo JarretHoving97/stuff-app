d42db68967e47eb04db592d93c30e14200f926e7
Jarret Hoving <jarret.hoving97@gmail.com>
2024-11-20 21:38:59 +0100

    More view cleaning and code sparation

--- The Stuff App/Stuff UI/Views/StuffDetailView.swift
+++ The Stuff App/Stuff UI/Views/StuffDetailView.swift
@@ -18,112 +18,17 @@ struct StuffDetailView: View {
     @State private var viewOffSet: CGSize = .zero
     @State var didGesture: Bool = false
     
-    @Environment(\.sizeCategory) var sizeCategory
-    
     var body: some View {
         
-        let isCompact = UIScreen.main.bounds.height <= 736
-        
         VStack(spacing: 20) {
-            VStack(spacing: 20) {
-                Text("What you'd like to do with this item?")
-                    .font(.title)
-                    .minimumScaleFactor(0.01)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .frame(height: 80)
-                    .lineLimit(3, reservesSpace: false)
-                    .foregroundStyle(.white)
-                    .bold()
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 24))
-                
-                ZStack {
-                    Color(.itemPrimary)
-                        .cornerRadius(20)
-                    
-                    Text(viewModel.item.name)
-                        .foregroundStyle(.white)
-                        .frame(alignment: .leading)
-                        .font(.title)
-                        .minimumScaleFactor(0.01)
-                        .fontWeight(.semibold)
-                        .padding()
-
-                    // n
-                    VStack {
-                        Spacer() // Pushes the RoundedRectangle to the bottom
-                        RoundedRectangle(cornerRadius: 12)
-                            .frame(width: 100, height: 8)
-                            .padding(.bottom, 8)// Optional padding if needed
-                            .foregroundStyle(Color.black.opacity(0.4))
-                    }
-                }
-                .offset(x: 0, y: min(viewOffSet.height + dragOffSet.height, 80))
-                .frame(height: isCompact ? 150 : 190)
-                .matchedGeometryEffect(id: viewModel.item.id, in: animation)
-                .simultaneousGesture(
-                    DragGesture()
-                        .onChanged { gesture in
-                            guard gesture.translation.height > -100 else {
-                                withAnimation(.spring(duration: 0.4)) {
-                                    viewOffSet = .zero
-                                    dragOffSet = .zero
-                                }
-                                return
-                            }
-                            dragOffSet = gesture.translation
-                            
-                            guard gesture.translation.height >= 80, !didGesture else { return }
-                            
-                            let generator = UIImpactFeedbackGenerator(style: .rigid)
-                            generator.impactOccurred()
-                            
-                            didGesture = true
-                        }
-                    
-                        .onEnded { gesture in
-                            
-                            if gesture.translation.height > 50  {
-                                
-                                withAnimation(.spring(duration: 1.3)) {
-                                    
-                                    showActions = false
-                                }
-                                
-                                withAnimation(.spring(duration: 0.1)) {
-                                    viewOffSet = .zero
-                                    dragOffSet = .zero
-                                }
-                                
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                                    viewModel.showClosingAnimaiton = false // never show repeating animation again
-                                    onClose?()
-                                }
-                            } else {
-                                withAnimation(.spring(duration: 0.4)) {
-                                    viewOffSet = .zero
-                                    dragOffSet = .zero
-                                }
-                                didGesture = false
-                            }
-                        }
-                )
-                
-                actionsGridView
-            }
-        
-            .onAppear {
-                withAnimation(.spring(duration: 0.6, bounce: 0.2)) {
-                    showActions = true
-                }
-            }
-            
-            Spacer()
+            largeTextView
+            swipableCardView
+            actionsGridView
         }
-        .padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(.bg).ignoresSafeArea(.all))
-        
         .onAppear {
+            withAnimation(.spring(duration: 0.6, bounce: 0.2)) {
+                showActions = true
+            }
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
                 if viewModel.showClosingAnimaiton {
@@ -133,11 +38,99 @@ struct StuffDetailView: View {
                     ) {
                         viewOffSet.height = 10
                     }
-                    
                 }
             }
-            
         }
+        .padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(.bg).ignoresSafeArea(.all))
+    }
+    
+    private var largeTextView: some View {
+        Text("What you'd like to do with this item?")
+            .font(.title)
+            .minimumScaleFactor(0.01)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .frame(height: 80)
+            .lineLimit(3, reservesSpace: false)
+            .foregroundStyle(.white)
+            .bold()
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 24))
+    
+    }
+    
+    private var swipableCardView: some View {
+        ZStack {
+            Color(.itemPrimary)
+                .cornerRadius(20)
+            
+            Text(viewModel.item.name)
+                .foregroundStyle(.white)
+                .frame(alignment: .leading)
+                .font(.title)
+                .minimumScaleFactor(0.01)
+                .fontWeight(.semibold)
+                .padding()
+
+            // n
+            VStack {
+                Spacer() // Pushes the RoundedRectangle to the bottom
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(width: 100, height: 8)
+                    .padding(.bottom, 8)// Optional padding if needed
+                    .foregroundStyle(Color.black.opacity(0.4))
+            }
+        }
+        .offset(x: 0, y: min(viewOffSet.height + dragOffSet.height, 80))
+        .frame(maxHeight: 190)
+        .matchedGeometryEffect(id: viewModel.item.id, in: animation)
+        .simultaneousGesture(
+            DragGesture()
+                .onChanged { gesture in
+                    guard gesture.translation.height > -100 else {
+                        withAnimation(.spring(duration: 0.4)) {
+                            viewOffSet = .zero
+                            dragOffSet = .zero
+                        }
+                        return
+                    }
+                    dragOffSet = gesture.translation
+                    
+                    guard gesture.translation.height >= 80, !didGesture else { return }
+                    
+                    let generator = UIImpactFeedbackGenerator(style: .rigid)
+                    generator.impactOccurred()
+                    
+                    didGesture = true
+                }
+            
+                .onEnded { gesture in
+                    
+                    if gesture.translation.height > 50  {
+                        
+                        withAnimation(.spring(duration: 1.3)) {
+                            
+                            showActions = false
+                        }
+                        
+                        withAnimation(.spring(duration: 0.1)) {
+                            viewOffSet = .zero
+                            dragOffSet = .zero
+                        }
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            viewModel.showClosingAnimaiton = false // never show repeating animation again
+                            onClose?()
+                        }
+                    } else {
+                        withAnimation(.spring(duration: 0.4)) {
+                            viewOffSet = .zero
+                            dragOffSet = .zero
+                        }
+                        didGesture = false
+                    }
+                }
+        )
     }
     
     private var actionsGridView: some View {
@@ -183,6 +176,8 @@ struct StuffDetailView: View {
                 
             }
             .frame(maxWidth: .infinity)
+            
+            Spacer()
         }
         .zIndex(-1)
         .frame(minHeight: 360)
